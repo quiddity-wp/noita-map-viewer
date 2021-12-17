5f9e269b7aaf34bd1c3378f303269d4764fc01a3
Aaron <admin@datahoarder.dev>
2021-12-17 04:56:53 +0100

    Add OpenSeadragonZoomLevels plugin
    Signed-off-by: Aaron <admin@datahoarder.dev>

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,9 @@ var viewer = OpenSeadragon({
   id: "osd-viewer",
   minZoomLevel: 0,
   defaultZoomLevel: 0,
-  tileSources: ["https://noita.datahoarder.dev/iiif3/info.json"]
+  tileSources: [""]
+});
+
+var zoomLevels = viewer.zoomLevels({
+  levels: [0, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 });
